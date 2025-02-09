29890fbc3a1b37ca94dafa2f3aa1e93dbea892e1
dylandru <158249525+dylandru@users.noreply.github.com>
2025-02-09 17:38:56 -0500

    Add Setup File to make pip installable

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="YOLO9",
+    version="0.1.0",
+    author="Wong Kin Yiu",
+    author_email="", 
+    description="A YOLO-based object detection implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dylandru/yolov9", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "gitpython",
+        "ipython",
+        "matplotlib>=3.2.2",
+        "numpy>=1.18.5", 
+        "opencv-python>=4.1.1",
+        "Pillow>=7.1.2",
+        "psutil",
+        "PyYAML>=5.3.1",
+        "requests>=2.23.0",
+        "scipy>=1.4.1",
+        "thop>=0.1.1",
+        "torch>=1.7.0",
+        "torchvision>=0.8.1",
+        "tqdm>=4.64.0",
+        "tensorboard>=2.4.1",
+        "pandas>=1.1.4",
+        "seaborn>=0.11.0",
+        "albumentations>=1.0.3",
+        "pycocotools>=2.0"
+    ],
+    include_package_data=True,
+)

770b9701471362b72d383b5f7d3ff02ef5971497
dylandru <158249525+dylandru@users.noreply.github.com>
2025-02-18 18:22:58 -0500

    Update repository structure for pip install

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -12,10 +12,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dylandru/yolov9",
-    packages=find_namespace_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
